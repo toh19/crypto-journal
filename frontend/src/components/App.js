7664b5d183934ac9bb6efa0f05f958c8f7abdf1b
toh19 <tarek.hamoud@gmail.com>
2023-10-19 21:11:42 -0400

    refactor: update project folder structure

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,3 +1,3 @@
-export function App() {
+export default function App() {
   return <h1>Hello Crypto Journal React 18</h1>
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
+import './styles/main.css';
 
-import App from './App';
+import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
