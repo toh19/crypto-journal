3bcb9d770c65e943f536b5355484263e5767733e
toh19 <tarek.hamoud@gmail.com>
2023-10-25 04:55:27 -0400

    update footer content

--- frontend/cj/src/components/Footer.tsx
+++ frontend/cj/src/components/Footer.tsx
@@ -1,9 +1,50 @@
+import Image from 'next/image'
+import logo from '../../public/crypto.png'
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <hr className="mt-5 mb-4" />
-      <p className="text-muted">Crypto Journal © 2023.</p>
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
+      <a
+        href="/"
+        className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+      >
+        <Image
+          src={logo}
+          alt="Picture of the author"
+          // width={100} // automatically provided
+          // height={100} // automatically provided
+          // blurDataURL="data:..." //automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
+      </a>
+      <ul className="nav col-md-4 justify-content-end">
+        <li className="nav-item">
+          <a href="#" className="nav-link px-2 text-body-secondary">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link px-2 text-body-secondary">
+            Features
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link px-2 text-body-secondary">
+            Pricing
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link px-2 text-body-secondary">
+            FAQs
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#" className="nav-link px-2 text-body-secondary">
+            About
+          </a>
+        </li>
+      </ul>
     </footer>
   )
 }
