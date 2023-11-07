cb8c4b043de50df2a92640f9f16646dcd225e95c
toh19 <tarek.hamoud@gmail.com>
2023-11-07 17:18:57 -0500

    add /users route

--- nosql/app.js
+++ nosql/app.js
@@ -12,10 +12,36 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => console.log('Connected to MongoDB')) 
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  firstName: String,
+  lastName: String,
+  isActive: { type: Boolean, default: true },
+  isVerified: { type: Boolean, default: false },
+  role: { type: String, default: 'user' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model('User', userSchema);
+
 // Simple route to test if server is working
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body)
+    await newUser.save()
+    res.status(201).send(newUser)
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
